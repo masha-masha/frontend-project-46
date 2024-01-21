f048fc9f38457a369e4f7839f1d2495760efc4ce
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-01-22 02:36:55 +0300

    fix some files

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ import getDiffTree from './getDiffTree.js';
 import getFormatStylish from './formatters/formatStylish.js';
 
 const readFile = (file) => {
-const way = path.resolve('./__fixtures__', file);
-const extname = path.extname(file).slice(1);
-const read = fs.readFileSync(way, {encoding: 'utf-8'})
-return parse(read, extname);
+  const way = path.resolve('./__fixtures__', file);
+  const extname = path.extname(file).slice(1);
+  const read = fs.readFileSync(way, {
+    encoding: 'utf-8'
+  })
+  return parse(read, extname);
 
 };
 
 const genDiff = (file1, file2) => {
   const data1 = readFile(file1);
   const data2 = readFile(file2);
- return getFormatStylish(getDiffTree(data1, data2));
+  const diffTree = getDiffTree(data1, data2)
+  return getFormatStylish(diffTree);
 };
 
 export default genDiff;
-
 
