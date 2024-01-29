c2fd962b0015bc3bfba87da0fec4c437d150e245
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-01-30 01:21:20 +0300

    index.js fixed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
+/* eslint no-underscore-dangle: 0 */
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import parse from './parsers.js';
 import getDiffTree from './getDiffTree.js';
 import getFormat from './formatters/index.js';
 
-const readFile = (file) => {
-  const way = path.resolve('./__fixtures__', file);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dataFromFile = (file) => {
+  const filePath = path.join(__dirname, '..', '__fixtures__', file);
   const extname = path.extname(file).slice(1);
-  const read = fs.readFileSync(way, { encoding: 'utf-8' });
+  const read = fs.readFileSync(filePath, { encoding: 'utf-8' });
   return parse(read, extname);
 };
 
 const genDiff = (file1, file2, format = 'stylish') => {
-  const data1 = readFile(file1);
-  const data2 = readFile(file2);
+  const data1 = dataFromFile(file1);
+  const data2 = dataFromFile(file2);
   const diffTree = getDiffTree(data1, data2);
   return getFormat(diffTree, format);
 };
