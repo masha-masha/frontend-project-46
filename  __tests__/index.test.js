3abbb551312bc3c7daf63057b9a107f528956fe0
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-01-30 01:33:41 +0300

    try to handle with tests

---  __tests__/index.test.js
+++  __tests__/index.test.js
@@ -14,20 +14,20 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 describe('output', () => {
   test('stylish works', () => {
     const expectedStylish = readFile('stylishFormatTest.txt');
-    const file1 = getFixturePath('file1.json');
-    const file2 = getFixturePath('file2.yaml');
+    const file1 = 'file1.json';
+    const file2 = 'file2.yaml';
     expect(genDiff(file1, file2, 'stylish')).toEqual(expectedStylish);
   });
   test('plain works', () => {
     const expectedPlain = readFile('plainFormatTest.txt');
-    const file1 = getFixturePath('file1.json');
-    const file2 = getFixturePath('file2.json');
+    const file1 = 'file1.json';
+    const file2 = 'file2.json';
     expect(genDiff(file1, file2, 'plain')).toEqual(expectedPlain);
   });
   test('json works', () => {
     const expectedJson = readFile('jsonFormatTest.txt');
-    const file1 = getFixturePath('file1.yaml');
-    const file2 = getFixturePath('file2.json');
+    const file1 = 'file1.yaml';
+    const file2 = 'file2.json';
     expect(genDiff(file1, file2, 'json')).toEqual(expectedJson);
   });
 });
