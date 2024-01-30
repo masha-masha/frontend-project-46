7fa613cb24e592b5e560af9b3b83f622a3865676
Maria Bazhenova <yankeevsdixie@gmail.com>
2024-01-31 01:45:11 +0300

    func renamed from getFixturesPath to getPath

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
 
 export default readFile;
